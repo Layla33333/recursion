7b4b20c134f0c32ac869d5c55cc587f98d898e66
layla <lkessler3@cnm.edu>
2021-11-01 07:34:10 -0600

    Extra credit

--- src/main/java/edu/cnm/deepdive/Factorials.java
+++ src/main/java/edu/cnm/deepdive/Factorials.java
@@ -1,14 +1,29 @@
 package edu.cnm.deepdive;
 
+
+import java.math.BigInteger;
+
 public abstract class Factorials {
 
-  public static long computeRecursive(int n) throws IllegalArgumentException {
+  public static BigInteger computeRecursive(int n) throws IllegalArgumentException {
     if (n < 0) {
       throw new IllegalArgumentException();
     }
-    return (n == 0)
-        ? 1
-        : (n * computeRecursive(n - 1));
+    if (n == 0) {
+      return new BigInteger("1");
+    } else {
+      return BigInteger.valueOf(n).multiply(computeRecursive(n - 1));
+    }
   }
 
+  public static BigInteger computeIterative(int n) throws IllegalArgumentException {
+    if (n < 0) {
+      throw new IllegalArgumentException("Parameter must be greater than or equal to zero");
+    }
+    BigInteger factorial = BigInteger.ONE;
+    for (int i = n; i > 0; i--) {
+       factorial = factorial.multiply(BigInteger.valueOf(i));
+    }
+    return factorial;
+  }
 }
